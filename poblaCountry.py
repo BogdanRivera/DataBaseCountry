5d5280e053578abe00bb77d886757372cb475121
Bogdan Rivera <bugy1998@hotmail.com>
2023-01-05 14:50:10 -0600

    Creación del proyecto

---
+++ poblaCountry.py
@@ -0,0 +1,7 @@
+"""
+Proyecto creado por Bogdan Rivera 
+Este trabajo se enfoca en leer un archivo de tipo csv para
+poder encontrar un país en específico. Una vez teniendo eso 
+se grafica el censo poblacional de cierto tiempo.
+"""
+
