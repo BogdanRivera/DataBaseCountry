99725df79635888faa61742bb14c11579db47cb4
Bogdan Rivera <bugy1998@hotmail.com>
2023-01-05 15:09:46 -0600

    Se realizó en la rama lectura el archivo .py de la lectura csv

---
+++ importa.py
@@ -0,0 +1,13 @@
+import csv
+
+
+def lee_csv(path):
+    with open(path, r) as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        header = next(reader)
+        datos = []
+        for row in reader:
+            dicc = {key: value for key, value in zip(header, reader)}
+            datos.append(dicc)
+    print(datos[0])
+    return datos
