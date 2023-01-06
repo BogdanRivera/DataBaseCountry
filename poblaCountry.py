8d613abeb5b57c498a79797dd819b27ccd235252
Bogdan Rivera <bugy1998@hotmail.com>
2023-01-05 18:41:28 -0600

    Cambio de todos los elementos a una sola función. Principal poblaCountry.py

--- poblaCountry.py
+++ poblaCountry.py
@@ -4,13 +4,57 @@ Este trabajo se enfoca en leer un archivo de tipo csv para
 poder encontrar un país en específico. Una vez teniendo eso 
 se grafica el censo poblacional de cierto tiempo.
 """
+import csv
+import matplotlib.pyplot as plt
 
-import buscaCSV
-import importa
+
+def lee_csv(path):
+    with open(path, 'r') as file:
+        # Lee el archivo CSV con delimitador en comas (,)
+        reader = csv.reader(file, delimiter=',')
+        # Ubica únicamente la primera fila y la guarda en un arreglo
+        header = next(reader)
+        paises = []
+        for row in reader:
+            item = zip(header, row)  # Contatena el header con las filas
+            # Realiza un cambio a diccionarios
+            dict_paises = {country: i for country, i in item}
+            paises.append(dict_paises)
+    return paises
+
+
+def find_country(country, data):
+    for i in data:
+        if i['Country/Territory'] == country:
+            return i
+    # Columna 5 hasta la 13
+
+
+def datos_country(pais):
+    valores = list(pais.values())
+    #  key = list(pais.keys())
+    llaves = ['2022', '2020', '2015', '2010',
+              '2000', '1990', '1980', '1970']
+    population = valores[5:13]
+    poblacion = [int(x) for x in population]
+    return llaves, poblacion
+
+
+def grafics(labels, values):
+    fig, ax = plt.subplots()
+    ax.bar(labels, values)
+    plt.show()
 
 
 def run():
-    print(None)
+    paises = lee_csv("./data.csv")
+    pais = input("Ingrese un país: ").capitalize()
+    country = find_country(pais, paises)
+    if country != None:
+        keys, values = datos_country(country)
+        grafics(keys, values)
+    else:
+        print("País no encontrado en la base de datos")
 
 
 if __name__ == "__main__":
